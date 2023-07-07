efd86d2bed8e78f23e649105fe461fcf7749da93
iagocq <18238046+iagocq@users.noreply.github.com>
2023-07-07 17:05:36 -0300

    write tar to emmc

--- sources/multistrap/box-tools/flasher.sh
+++ sources/multistrap/box-tools/flasher.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+BOOT_DEBUG=5
 . /usr/local/box-tools/utils.sh
+set -x
+
+sleep 30
 
 if [ -e "/mnt/flash.conf" ]; then
     source /mnt/flash.conf
@@ -25,13 +29,13 @@ done
 file_size=0
 file_command=""
 
-rm -f comm comm2
-mkfifo comm comm2
+rm -f /tmp/comm /tmp/comm2
+mkfifo /tmp/comm /tmp/comm2
 
 if [ $? = 2 ]; then
     echo "no ip"
-elif [ "$server_ip" -a "$server_port" ];
-    nc "$server_ip" "$server_port" < comm &
+elif [ "$server_ip" -a "$server_port" ]; then
+    nc "$server_ip" "$server_port" < /tmp/comm &
     nc_proc=$!
 fi
 
@@ -39,6 +43,12 @@ get_file_info() {
     if [[ "$file" == http* ]]; then
         file_size=$(curl -LsI "$file" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
         file_command="curl -Ls '$file'"
+    elif [[ "$file" == *.tar.gz ]]; then
+        tar_process=1
+        tar_cmd="cat '$file' | gunzip | tar xvf - -C /tmp/flash --warning=no-timestamp"
+    elif [[ "$file" == *.tar ]]; then
+        tar_process=1
+        tar_cmd="tar xvf '$file' -C /tmp/flash --warning=no-timestamp"
     elif [[ "$file" == *.gz ]]; then
         file_size=$(gunzip -l "$file" | tail -1 | awk '{print $2}')
         file_command="cat '$file' | gunzip"
@@ -57,14 +67,28 @@ while [ "$file_size" = "" ]; do
     get_file_info
 done
 
-eval $file_command | pv -b -n > >(dd of=$output conv=fsync 2> /dev/null) 2> comm2 &
+if [ "$tar_process" ]; then
+    parted "$output" -- mklabel msdos mkpart 8192 100%
+    device="$(losetup -o $((8192*512)) --find --show "$output")"
+    mkfs.ext4 -F $device
+    mkdir /tmp/flash
+    mount "$device" /tmp/flash
+    unset file_size
+    eval $tar_cmd | grep -Eo '^./[a-z0-9\-_]+/[a-z0-9\-_]+' | stdbuf --output=L uniq > /tmp/comm2 &
+else
+    eval $file_command | pv -b -n > >(dd of=$output conv=fsync 2> /dev/null) 2> /tmp/comm2 &
+fi
 
 sts=0
 while [ $sts = 0 ]; do
     read
     [ -z "$REPLY" ] && echo done && break
     echo progress $REPLY $file_size
-done < comm2 >> comm
+done < /tmp/comm2 >> /tmp/comm
+
+if [ "$tar_process" ]; then
+    umount /tmp/flash
+fi
 
 echo done
 sleep 1
--- sources/multistrap/box-tools/utils.sh
+++ sources/multistrap/box-tools/utils.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
+BOOT_DEBUG=5
 if [ -z "$BOOT_DEBUG" ]; then
-    BOOT_DEBUG=0
+    BOOT_DEBUG=5
 fi
 
 # debug code inspired by debian's initramfs init
 debugshell() {
-    if [ "$BOOT_DEBUG" -gt 3 ]; then
+    if [ "$BOOT_DEBUG" -gt 2 ]; then
         echo "debugshell: $1"
         setsid bash
     fi
 }
 
-if [ "$BOOT_DEBUG" -gt 1 ]; then
+if [ "$BOOT_DEBUG" -gt 0 ]; then
     set -x
 fi
